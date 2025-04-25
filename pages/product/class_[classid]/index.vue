e05d57c18cddef72112acce91e033de8e674d1c0
paul0127 <paul@eso.com.tw>
2025-04-25 20:27:55 +0800

    0425

--- pages/product/class_[classid]/index.vue
+++ pages/product/class_[classid]/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import pager from '~/components/Common/pager.vue'
+import pager from '~/components/common/pager.vue'
 import Title from '~/components/common/Title.vue'
 import { currency } from '~/utils/filter.js'
 
